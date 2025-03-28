8f4f0c4d04dcc953b13de25de5902e703d756dcb
Anuar24399 <43638208+Anuar24399@users.noreply.github.com>
2025-03-29 00:44:21 +0100

    Delete T directory

--- T/drop.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = new PDO('sqlite:' . __DIR__ . '/data.db');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$db->exec("DROP TABLE IF EXISTS logins");
-$db->exec("DROP TABLE IF EXISTS ing_logins");
-
-echo "Tabellen 'logins' en 'ing_logins' verwijderd!";
-?>
